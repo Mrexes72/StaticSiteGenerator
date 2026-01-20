3b06946c3a4abba4bd9d1f443aa2675c029138a5
Mrexes72 <per.lund93@gmail.com>
2026-01-20 10:28:47 +0100

    første commit

---
+++ src/textnode.py
@@ -0,0 +1,24 @@
+from enum import Enum
+
+class TextType(Enum):
+    TEXT = "text"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINK = "link"
+    IMAGE = "image"
+
+class TextNode:
+    def __init__(self, text: str, text_type: TextType, url: str = None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, other) -> bool:
+        return (self.text == TextNode.text and
+                self.text_type == TextNode.text_type and
+                self.url == TextNode.url)
+    
+    def __repr__(self) -> str:
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+    
