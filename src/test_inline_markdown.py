e7d7fd60b92fa80917e4deac2632bacd4a0de8f3
Mrexes72 <per.lund93@gmail.com>
2026-01-26 12:34:40 +0100

    adde functions for extracting markdwon links and images with tests

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,8 +1,8 @@
 import unittest
 from textnode import TextNode, TextType
-from inline_markdown import split_nodes_delimiter
+from inline_markdown import split_nodes_delimiter, extract_markdown_images, extract_markdown_links
 
-class TestSplitNodesDelimiter(unittest.TestCase):
+class TestInlineMarkdown(unittest.TestCase):
     def test_split_simple_code(self):
         node = TextNode("hello `world`!", TextType.TEXT)
         result = split_nodes_delimiter([node], "`", TextType.CODE)
@@ -51,6 +51,52 @@ class TestSplitNodesDelimiter(unittest.TestCase):
         self.assertEqual(result[0], TextNode("hello ", TextType.TEXT))
         self.assertEqual(result[1], TextNode("world", TextType.BOLD))
         self.assertEqual(result[2], TextNode("!", TextType.TEXT))
+
+    def test_single_image(self):
+        text = "Here is an image ![alt](https://example.com/img.png)"
+        result = extract_markdown_images(text)
+        self.assertEqual(result, [("alt", "https://example.com/img.png")])
+
+    def test_multiple_images(self):
+        text = "![one](url1) and also ![two](url2)"
+        result = extract_markdown_images(text)
+        self.assertEqual(result, [
+            ("one", "url1"),
+            ("two", "url2"),
+        ])
+
+    def test_no_images(self):
+        text = "This text has no images"
+        result = extract_markdown_images(text)
+        self.assertEqual(result, [])
+
+    def test_image_with_empty_alt(self):
+        text = "![](url)"
+        result = extract_markdown_images(text)
+        self.assertEqual(result, [("", "url")])
+    
+    def test_single_link(self):
+        text = "Click [here](https://example.com)"
+        result = extract_markdown_links(text)
+        self.assertEqual(result, [("here", "https://example.com")])
+
+    def test_multiple_links(self):
+        text = "A [one](url1) and [two](url2)"
+        result = extract_markdown_links(text)
+        self.assertEqual(result, [
+            ("one", "url1"),
+            ("two", "url2")
+        ])
+
+    def test_no_links(self):
+        text = "No links here!"
+        result = extract_markdown_links(text)
+        self.assertEqual(result, [])
+
+    def test_does_not_match_images(self):
+        text = "Look at this image ![alt](url)"
+        result = extract_markdown_links(text)
+        self.assertEqual(result, [])  # must NOT include image
 
 
 if __name__ == "__main__":
